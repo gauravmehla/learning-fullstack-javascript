4e00725b14307252d594a306aabcf36960689c71
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-06-27 22:17:03 +0530

    Express server ready. Add EJS template language.

---
+++ server.js
@@ -0,0 +1,22 @@
+import config from './config';
+import apiRouter from './api';
+
+import express from 'express';
+const server = express();
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+	res.render('index',{
+		content : 'Welcome to express.'
+	});
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+
+
+server.listen(config.port, () => {
+	console.log('Express listening on http://localhost:' + config.port);
+});
