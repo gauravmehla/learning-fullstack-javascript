c358496d0068c376291febd1fd3a79d8dc5f1395
Gaurav Mehla <gaurav.ror007@gmail.com>
2017-06-28 07:53:42 +0530

    Css in React #example

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' : 'red';
+
 ReactDOM.render(
-	React.createElement('h2', null, 'Hello React!'),
+	<h2 style={{ color }}>Hello React with JSX!!</h2>,
 	document.getElementById('root')
 );
